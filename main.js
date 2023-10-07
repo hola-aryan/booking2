67cff30971c0305623ef487a5f50519bc832235a
hola-aryan <aryanisastudent@gmail.com>
2023-10-07 09:24:00 +0530

    Booking 2 with local Storage

---
+++ main.js
@@ -0,0 +1,40 @@
+var nameInput = document.getElementById('name');
+var emailInput = document.getElementById('email');
+var phoneInput = document.getElementById('phone');
+var myForm = document.getElementById('my-form');
+var localStorageDataList = document.getElementById("localStorageData");
+
+myForm.addEventListener('submit',addItem);
+
+function addItem(e){
+    e.preventDefault();
+
+    // Get user input values
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const phone = phoneInput.value;
+
+    const userData = { name, email, phone};
+
+    localStorage.setItem(email, JSON.stringify(userData));
+
+    nameInput.value = "";
+    emailInput.value = "";
+    phoneInput.value="";
+
+    displayLocalStorageData();
+}
+
+function displayLocalStorageData() {
+    localStorageDataList.innerHTML = ""; // Clear previous data
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const email = localStorage.key(i);
+        const userData = JSON.parse(localStorage.getItem(email));
+
+        const listItem = document.createElement("li");
+        listItem.textContent = `Name: ${userData.name}, Email: ${userData.email}, Mobile: ${userData.phone}`;
+
+        localStorageDataList.appendChild(listItem);
+    }
+}
